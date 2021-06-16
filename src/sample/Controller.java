f952556b3eed4c628159a59da4ef04def12e0381
timofeya123 <timofeya123@inbox.ru>
2021-06-16 10:01:57 +0600

    comment

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,107 +18,152 @@ import java.util.ArrayList;
 
 public class Controller {
 
-    ArrayList<String> text = new ArrayList<>();
-    ArrayList<String> wodrs = new ArrayList<>();
-    ArrayList<WordCount> wordCounts = new ArrayList<>();
+    // коллекции данных
+    ArrayList<String> text = new ArrayList<>();  // полученные и необработанные строчки
+    ArrayList<String> wodrs = new ArrayList<>(); // разбитые на слова
+    ArrayList<WordCount> wordCounts = new ArrayList<>(); // подсчитанные слова
 
-    @FXML TextField nameURL;
-    @FXML TextArea phrases;
-    @FXML Label status;
+    // внешние эл. в отображаемой часте
+    @FXML TextField nameURL;  // ввод нашего URL
+    @FXML TextArea phrases; // вывод наших значений
+    @FXML Label status; // статус действий
 
 
     // ---------------------------------------------------------------------- инициализация ----------------------
     public void init(){
         nameURL.setText("https://www.simbirsoft.com/");
-    }
+    } // ссылка по умолчанию
 
     public void start(MouseEvent event) throws Exception {
-        status.setText(" Статус: попытка соединения");
-        connect();
-        status.setText(" Статус: обработка текста");
-        treatmentParse();
-        treatmentCount();
-        status.setText(" Статус: успех");
-        for (WordCount w:wordCounts){
-            //System.out.println(w.getWord()+"-"+w.getCount());
-            phrases.setText(phrases.getText()+w.getWord()+"-"+w.getCount()+"\n");
+
+        zeroing(); // команда обнуления
+        status.setText(" Статус: попытка соединения"); // статус
+
+        if(connectURL()){ // соединение
+            status.setText(" Статус: обработка текста");// статус
+            wodrs = treatmentParse(text); // разбиение на слова (парсим текст)
+
+            wordCounts = treatmentCount(wodrs); // подсчёт слов
+            status.setText(" Статус: подсчёт слов, успех!");// статус
+
+            for (WordCount w:wordCounts){ // TextArea, циклк для вывода наших значений
+                //System.out.println(w.getWord()+"-"+w.getCount());
+                phrases.setText(phrases.getText()+w.getWord()+"-"+w.getCount()+"\n");
+            }
         }
     }
 
-    private void connect() throws Exception{
+    // ---------------------------------------------------------------------- команда обнуления ----------------------
+    private void zeroing() throws Exception{
+        text = new ArrayList<>();
+        wodrs = new ArrayList<>();
+        wordCounts = new ArrayList<>();
+        status.setText("");
+        phrases.setText("");
+    }
+
+    // ---------------------------------------------------------------------- соединение с URL ----------------------
+    private boolean connectURL() throws Exception{
         try {
-            URL url = new URL(nameURL.getText());
-
-            URLConnection urlc = url.openConnection();
+            URL url = new URL(nameURL.getText()); // закидываем нашу ссылку
+            URLConnection urlc = url.openConnection(); // открыть соединение
             urlc.setRequestProperty("User-Agent", "Mozilla 5.0 (Windows; U; "
-                    + "Windows NT 5.1; en-US; rv:1.8.0.11) ");
-            InputStream inputFile = urlc.getInputStream();
+                    + "Windows NT 5.1; en-US; rv:1.8.0.11) "); // дефолтные настройки браузера соединения
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputFile, "UTF-8"));
+            InputStream inputFile = urlc.getInputStream(); // стрим данных приёма
+            status.setText(" Статус: соединение, успех"); // статус
 
-            while (true) {
-                String line = reader.readLine();
-                if (line == null)
-                    break;
-                text.add(line);
-            }
-            status.setText(" Статус: соединение, успех");
+            readerURLtoText(inputFile); // считывание текста из URL
+            status.setText(" Статус: считывание текста из URL, успех"); // статус
+            return true;
+
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            status.setText(" Статус: Error");
+            System.out.println(e.getMessage()); // ошибка (￣ ￣|||)
+            status.setText(" Статус: Error"); // статус
+            return false;
         }
     }
 
-    private void treatmentParse() throws Exception{
-        for (String s:text){
-            //s = s.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+    // ---------------------------------------------------------------------- считывание текста из URL
+    private void readerURLtoText(InputStream inputFile) throws Exception{
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputFile, "UTF-8")); //буфер чтения
 
-            //внутренний массив с которым ми работаем
-            char[] digits = new char[]{' ', ',', '.', '!', '?', '"',';',':','[',']','(',')','\n','\r','\t', '/', '\\', '<', '>', '=', '\'', '_', '|'};
-            String word=s, newWord="";
-            boolean fitsWord = false;
+            while (true) { // цикл на чтение данных из буфера, по строчке
+                String line = reader.readLine();
+                if (line == null) // если строка ничего не имеет
+                    break; // закончить
+                text.add(line); // запись полученой сроки в коллекцию строк
+            }
+        } catch (Exception e) {
+        System.out.println(e.getMessage()); // ошибка (￣ ￣|||)
+        status.setText(" Статус: Error"); // статус
+        }
+    }
 
-            for (int i=0;i<word.length();i++){
+    // ---------------------------------------------------------------------- разбиение на слова (парсим текст) ----------------------
+    private ArrayList<String> treatmentParse(ArrayList<String> text) throws Exception{
+        ArrayList<String> arrayWodrs = new ArrayList<>();
+        try {
+            for (String s:text){
 
-                for (int j=0;j<digits.length;j++){
-                    if (word.charAt(i) == digits[j]) {
-                        fitsWord=false;
-                        j=digits.length;
+                //внутренний массив с которым ми работаем
+                char[] digits = new char[]{' ', ',', '.', '!', '?', '"',';',':','[',']','(',')','\n','\r','\t', '/', '\\', '<', '>', '=', '\'', '_', '|'};
+                String word=s, newWord=""; // присваеваем
+                boolean fitsWord = false;
+
+                for (int i=0;i<word.length();i++){ // цикл по перебору символов в строке
+                    for (int j=0;j<digits.length;j++){ // цикл по символам
+                        if (word.charAt(i) == digits[j]) { // совпали? (￢_￢)
+                            fitsWord=false; // тогда это не нужный сивол
+                            j=digits.length; // заканчиваем костылём, цикл, потому что можем (」°ロ°)」, да, да я знаю спец команды, но так эффектние!
+                        } else {
+                            fitsWord=true; // инаце это новый символ
+                        }
+                    }
+
+                    if(fitsWord==true){ // есть новый символ?
+                        newWord+=word.charAt(i); // до? добавь его в будущее слово! (」°ロ°)」
                     } else {
-                        fitsWord=true;
-                    }
-                }
+                        newWord = newWord.replaceAll("[^A-Za-zА-Яа-я0-9]", ""); // очишаем слово от лишнего (＃￣ω￣)
 
-                if(fitsWord==true){
-                    newWord+=word.charAt(i);
-                } else {
-                    newWord = newWord.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
-                    if(newWord!=""){
-                        wodrs.add(newWord);
-                        newWord="";
+                        if(newWord!=""){ // если слово есть, то ...
+                            arrayWodrs.add(newWord); // закидываем полученное слово в коллекцию
+                            newWord=""; // обнуляем новое слово
+                        }
                     }
                 }
 
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage()); // ошибка (￣ ￣|||)
+            status.setText(" Статус: Error"); // статус
 
         }
+        return arrayWodrs; // возвращаем коллекцию
     }
 
-    private void treatmentCount() throws Exception{
-        for (String s:wodrs){
-            boolean repeat=false;
-            if(wordCounts!=null){
-                for (WordCount w:wordCounts){
-                    if (w.getWord().equals(s)){
-                        repeat=true;
-                        w.setCount(w.getCount()+1);
+    // ---------------------------------------------------------------------- подсчёт слов ----------------------
+    private ArrayList<WordCount> treatmentCount(ArrayList<String> wodrs) throws Exception{
+        ArrayList<WordCount> wordCounts = new ArrayList<>();
+
+        for (String s:wodrs){ // цикл по перебору символов в строке
+            boolean repeat=false; // повторился
+
+            if(wordCounts!=null){ // если он не пустой
+                for (WordCount w:wordCounts){ // пройтись по новой коллекции
+                    if (w.getWord().equals(s)){ // сравнивая слово с нашими
+                        repeat=true; // нашли, повторение
+                        w.setCount(w.getCount()+1); // увеличили число  (＃￣ω￣)
                     }
                 }
             }
-            if (repeat==false){
-                wordCounts.add(new WordCount(s, 1));
+            if (repeat==false){ // не нашли? (￢_￢)
+                wordCounts.add(new WordCount(s, 1)); // добавили слово  (＃￣ω￣)
             }
         }
+
+        return wordCounts;// возвращаем коллекцию
     }
 
 
